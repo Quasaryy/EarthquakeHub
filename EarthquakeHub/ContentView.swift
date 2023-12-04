4e6381541a6ab409d5f6b115e53d88aa3a48a420
Yury Lebedev <quasaryy@gmail.com>
2023-12-04 19:53:59 +0400

    Test commit

--- EarthquakeHub/ContentView.swift
+++ EarthquakeHub/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
             Text("Oops! This is a commercial app, there is no code here.")
         }
         .padding()
