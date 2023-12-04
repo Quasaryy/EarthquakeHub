c25f1b88966c5a2250adcc0b6d09fb1f0cde8691
Yury Lebedev <quasaryy@gmail.com>
2023-12-04 18:21:05 +0400

    Initial Commit

---
+++ EarthquakeHub/EarthquakeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakeHubApp.swift
+//  EarthquakeHub
+//
+//  Created by Yury on 04.12.23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
